3677210965f38fcbdc5acde492890c140fc17daa
Dawson Toth <dtoth@appcelerator.com>
2011-03-01 12:02:45 -0600

    Added map of venue

--- Resources/jab11/views/maps/venueMap.js
+++ Resources/jab11/views/maps/venueMap.js
@@ -4,5 +4,26 @@ view = function(model) {
         left: AirView('homeButton', win),
         title: 'Venue Map'
     }));
+    win.add(Titanium.Map.createView({
+        top: 45,
+        mapType: Titanium.Map.STANDARD_TYPE,
+        region: {
+            latitude: 50.8683, longitude: 6.0818,
+            latitudeDelta: 0.01, longitudeDelta: 0.01
+        },
+        animate: true,
+        regionFit: true,
+        userLocation: true,
+        annotations: [
+            Titanium.Map.createAnnotation({
+                latitude: 50.8683,
+                longitude: 6.0818,
+                title: 'Rolduc Abbey',
+                subtitle: 'Kerkrade, Netherlands',
+                pincolor: Titanium.Map.ANNOTATION_RED,
+                animate: true
+            })
+        ]
+    }));
     return win;
 };
