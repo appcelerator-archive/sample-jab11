3221743f30b2e8ebe38913875bcf21eefbcb2898
Dawson Toth <dtoth@appcelerator.com>
2011-03-18 10:52:21 -0500

    Created basic settings window

--- Resources/controllers/settings.js
+++ Resources/controllers/settings.js
@@ -2,9 +2,6 @@ controller = {
     actions: {
         list: function() {
             return AirView();
-        },
-        addComment: function() {
-            return AirView();
         }
     }
 };
--- Resources/views/settings/list.js
+++ Resources/views/settings/list.js
@@ -1,3 +1,8 @@
-view = function() {
-    return AirView('notImplemented');
+view = function(model) {
+    var win = new View({ id: 'AboutWindow', className: 'Window' });
+    win.add(AirView('titleBar', {
+        left: AirView('button', { view: win, type: 'Home' }),
+        center: 'Settings'
+    }));
+    return win;
 };
