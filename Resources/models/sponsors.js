a6f2c78f96d434700decfbbe3e6394d0369d9ba1
fusion94 <tguntharp@appcelerator.com>
2011-04-25 15:45:53 -0700

    new icons, added more sponsors

--- Resources/models/sponsors.js
+++ Resources/models/sponsors.js
@@ -3,10 +3,10 @@ model = {
     subtitle: '6-8 May, 2011',
     bodyHTML: '<div>' +
     		  '<h3>Main Sponsor</h3><ul><li><a href="http://redcomponent.com/">redCOMPONENT.com</a></li></ul>' +
-    		  '<h3>Social Sponsor</h3><ul><li>eBay</li></ul>' +
-    		  '<h3>Gold Sponsors</h3><ul><li>joomlart<li>community builder<li>microsoft<li>mailchimp</li></ul>' +
-    		  '<h3>Silver Sponsors</h3><ul><li>joomfish<li>k2<li>sobipro<li>anything digital<li>nooku<li>appcelerator</li></ul>' +
-    		  '<h3>Bronze Sponsors</h3><ul><li>Yireo<li>JSeibold<li>Franzis Verlag<li>freakedout.de</li></ul>' +
+    		  '<h3>Social Sponsor</h3><ul><li><a href="http://www.ebay.com/">eBay</a></li></ul>' +
+    		  '<h3>Gold Sponsors</h3><ul><li><a href="http://www.joomlart.com/">JoomlArt</a><li><a href="http://www.joomlapolis.com/">Community Builder</a><li><a href="http://www.microsoft.com/web/joomla/">Microsoft</a><li><a href="http://www.mailchimp.com/">MailChimp</a></li></ul>' +
+    		  '<h3>Silver Sponsors</h3><ul><li><a href="http://www.jommfish.net/">JoomFish</a><li><a href="http://getk2.org/">K2</a><li><a href="http://www.sigsiu.net/sobipro.html">SobiPro</a><li><a href="http://www.anything-digital.com/">Anything Digital</a><li><a href="http://www.nooku.org/">Nooku</a><li><a href="http://appcelerator.com">Appcelerator</a></li></ul>' +
+    		  '<h3>Bronze Sponsors</h3><ul><li><a href="http://www.yireo.com/">Yireo</a><li>JSeibold<li>Franzis Verlag<li><a href="http://freakedout.de">freakedout.de</a></li></ul>' +
     		  '</div>'
     		  
 };
