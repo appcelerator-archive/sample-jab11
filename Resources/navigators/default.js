c9dc7c2215236eadcebcdea2d76019b5fb7f3e23
Dawson Toth <dtoth@appcelerator.com>
2011-05-05 12:23:06 -0400

    Removed animations from Android on navigator.

--- Resources/navigators/default.js
+++ Resources/navigators/default.js
@@ -29,6 +29,12 @@ navigator = {
         this.__mainWindow.open(this.__animations.fade.visible);
     },
     open: function(url, target, evt) {
+        if (Ti.Android) {
+            // if we're on Android, open the target straight away.
+            this.__mainWindow.add(target);
+            return;
+        }
+        // but on iPhone, we add some flair / animation to the opening.
         var animation = this.__animations.fade.visible;
         target.opacity = 0;
         this.__mainWindow.add(target);
@@ -36,9 +42,13 @@ navigator = {
             switch (target.navigatorOptions.animate) {
                 case 'pop':
                     animation = this.__animations.pop.visible;
+                    var x = (evt.globalPoint && evt.globalPoint.x && (evt.globalPoint.x - target.width / 2))
+                            || (Ti.Platform.DisplayCaps.platformWidth / 2);
+                    var y = (evt.globalPoint && evt.globalPoint.y && (evt.globalPoint.y - target.height / 2))
+                            || (Ti.Platform.DisplayCaps.platformHeight / 2);
                     target.poppedTransform = target.transform =
                             Ti.UI.create2DMatrix()
-                                    .translate(evt.globalPoint.x - target.width / 2, evt.globalPoint.y - target.height / 2)
+                                    .translate(x, y)
                                     .scale(0.1, 0.1);
                     break;
                 case 'tabSlide':
@@ -56,6 +66,12 @@ navigator = {
         });
     },
     close: function(target) {
+        if (Ti.Android) {
+            // if we're on Android, close the target straight away.
+            this.__mainWindow.remove(target);
+            return;
+        }
+        // but on iPhone, we add some flair / animation to the closing.
         var animation = this.__animations.fade.hidden;
         if (target.navigatorOptions) {
             switch (target.navigatorOptions.animate) {
