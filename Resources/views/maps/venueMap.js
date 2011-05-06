333ad0cf124b75e5196a7d8534787cd32f975b29
Dawson Toth <dtoth@appcelerator.com>
2011-05-05 21:47:23 -0400

    Venue map on Android implementation. Scroll views don't support pinch and zoom quite yet (dreadfully annoying, really).

--- Resources/views/maps/venueMap.js
+++ Resources/views/maps/venueMap.js
@@ -1,17 +1,25 @@
 view = function() {
     var win = new View({ className: 'Window' });
+    var map = new ImageView({
+        id: 'VenueMapImageView'
+    });
+    // Android doesn't support panning and pinching scroll views, so we'll add the image directly
+    if (Ti.Android) {
+        win.add(map);
+    }
+    else {
+        var scroll = new ScrollView({
+            top: 45,
+            id: 'VenueMapScroll'
+        });
+        scroll.add(map);
+        win.add(scroll);
+        scroll.zoomScale = scroll.minZoomScale;
+    }
+    // Note that we add the title bar last so that it will be on top of the map
     win.add(AirView('titleBar', {
         left: AirView('button', { view: win, type: 'Home' }),
         center: 'Venue Map'
     }));
-    var scroll = new ScrollView({
-        top: 45,
-        id: 'VenueMapScroll'
-    });
-    scroll.add(new ImageView({
-        id: 'VenueMapImageView'
-    }));
-    win.add(scroll);
-    scroll.zoomScale = scroll.minZoomScale;
     return win;
 };
