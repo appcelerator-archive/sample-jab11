a07c3d5c6f8074fa9140050dae0fa45b3bf5915f
Dawson Toth <dtoth@appcelerator.com>
2011-05-06 02:17:31 -0400

    Fixed viewport width of HTML.

--- Resources/views/shared/htmlHeader.js
+++ Resources/views/shared/htmlHeader.js
@@ -1,4 +1,5 @@
 view = '<html><head>'
+     + '<meta name="viewport" content="width=device-width, initial-scale=1.0"/>'
      + '<link rel="stylesheet" href="content/styles/k2.css" type="text/css" />'
      + '<link rel="stylesheet" href="content/styles/template.css" type="text/css" />'
      + '<link rel="stylesheet" href="content/styles/mobile.css" type="text/css" />'
